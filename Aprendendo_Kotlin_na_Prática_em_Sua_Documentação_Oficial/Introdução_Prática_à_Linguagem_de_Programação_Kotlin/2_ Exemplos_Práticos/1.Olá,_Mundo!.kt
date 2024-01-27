bf1fbc8a4689dc6fdc8e0de2842aa6f8f74bdbf6
Bee <estudos.da.debs@gmail.com>
2024-01-27 09:03:28 -0300

    aaaaaaaaaaaaaa

---
+++ Aprendendo_Kotlin_na_Prática_em_Sua_Documentação_Oficial/Introdução_Prática_à_Linguagem_de_Programação_Kotlin/2_ Exemplos_Práticos/1.Olá,_Mundo!.kt
@@ -0,0 +1,14 @@
+package org.kotlinlang.play         // 1. Definição do pacote (algo muito comum para organização de código)
+
+fun main() {                        // 2. Ponto de entrada de um código Kotlin, definido pela função [main]
+    println("Hello, World!")        // 3. Imprime na saída padrão um texto, pulando linha
+}
+
+// println faz pular linha, já o print apenas n pula
+
+package me.dio.helloworld
+
+fun main() {
+    println("Olá, Mundo!")
+}
+
